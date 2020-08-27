2149a28314666bfbf85fed26a63ec7a68114b841
Blair Cosgrove <57191365+Cosy8@users.noreply.github.com>
2020-08-27 10:13:00 -0400

    Update autoscrollbar.py

--- autoscrollbar.py
+++ autoscrollbar.py
@@ -12,5 +12,5 @@ class autoscrollbar(tk.Scrollbar):
                 self.pack(side=tk.RIGHT, fill=tk.Y)
             else:
                 self.pack(side=tk.BOTTOM, fill=tk.X)
-
+        self.master.update()
         tk.Scrollbar.set(self, low, high)
