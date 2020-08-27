5955f7bd85e06f1f0bd435c0304f3de8402ba516
Blair Cosgrove <57191365+Cosy8@users.noreply.github.com>
2020-08-27 10:13:19 -0400

    Print SQL calls

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import psycopg2, os
 #!  Credentials set in environment variables
 HOST =  os.environ['LIST_HOST']
 PORT = os.environ['LIST_PORT']
-DATABASE = os.environ['LIST_DATaBASE']
+DATABASE = os.environ['LIST_DATABASE']
 USER = os.environ['LIST_USER']
 PASSWORD = os.environ['LIST_PASSWORD']
 
@@ -16,12 +16,12 @@ class database:
     #   SQL select call
     #   table       -'FROM' statement
     #   columns     -'SELECT' statement columns
-    #   condition   -'WHERE' statement
     #   order       -'ORDER BY' statement
-    def select(self, table, columns='*', condition=0, order=0):
-        command = 'SELECT ' + columns + ' FROM ' + str(table)
+    def select(self, table, columns='*', order='item_id'):
+        command = 'SELECT ' + columns + ' FROM ' + str(table) + ' ORDER BY ' + order + ';'
         self.cur.execute(command)
         query = self.cur.fetchall()
+        print(command)
         return query
 
     #   SQL set call
@@ -32,6 +32,7 @@ class database:
         command = 'UPDATE ' + table + " SET item='" + str(value) + "' WHERE item='" + str(item) + "';"
         self.cur.execute(command)
         self.conn.commit()
+        print('\n' + command)
 
     #   SQL DELETE call
     #   table   -'FROM' statement
@@ -40,22 +41,25 @@ class database:
         command = 'DELETE FROM ' + table + " WHERE item='" + str(item) + "';"
         self.cur.execute(command)
         self.conn.commit()
+        print('\n' + command)
 
     #   SQL TRUNCATE call
     #   table   -'TUNCATE' statement
     def truncate(self, table):
-        command = 'TRUNCATE ' + str(table)
+        command = 'TRUNCATE ' + str(table) + ';'
         self.cur.execute(command)
         self.conn.commit()
+        print('\n' + command)
 
     #   SQL INSERT call
     #   table   -'FROM' statement
     #   value   -'VALUES' statement
     def insert(self, table, value):
         #*  Get the largest id in the database
-        command = 'SELECT MAX(item_id) FROM ' + str(table)
+        command = 'SELECT MAX(item_id) FROM ' + str(table) + ';'
         self.cur.execute(command)
         query = self.cur.fetchall()
+        print('\n' + command)
 
         if query[0][0]:
             max_id = query[0][0] + 1
@@ -63,13 +67,14 @@ class database:
             max_id = 1
 
         #*  Insert the new item with id = max + 1
-        command = 'INSERT INTO ' + str(table) + "(item_id, item, completed) VALUES (" + str(max_id) + ", '" + value + "',False);"
+        command = 'INSERT INTO ' + str(table) + "(item_id, item, completed) VALUES(" + str(max_id) + ", '" + value + "', False);"
         self.cur.execute(command)
         self.conn.commit()
+        print(command)
 
     #   Close the connection to the database
     def close(self):
-        print('Closing connection...')
+        print('\nClosing connection...')
         self.conn.close()
         self.cur.close()
         print('Connection closed')
